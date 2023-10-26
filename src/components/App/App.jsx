68d92af4530065603b8f0d9da010c3a0c4440d0a
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-10-26 19:14:07 +0300

    created folder for the component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { nanoid } from 'nanoid';
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { Filter } from './Filter/Filter';
+import { ContactForm } from '../ContactForm/ContactForm';
+import { ContactList } from '../ContactList/ContactList';
+import { Filter } from '../Filter/Filter';
+import css from './App.module.css';
 
 const LS_KEY = 'contacts';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
@@ -66,9 +72,9 @@ export class App extends Component {
     const { filter } = this.state;
     return (
       <div>
-        <h1>Phonebook</h1>
+        <h1 className={css.tatel}>Phonebook</h1>
         <ContactForm onSubmit={this.addNameContact} />
-        <h2>Contacts</h2>
+        <h2 className={css.tatel}>Contacts</h2>
         <Filter value={filter} onChangeFilter={this.changeFilter} />
         <ContactList
           contacts={this.getFilteredContacts()}
